aa310c58b70159ab262e3ec10f12050ee38a8d6b
Sazeebul Bashar <87577411+SazeebulBashar@users.noreply.github.com>
2023-12-06 19:08:58 +0600

    files uploaded

---
+++ practice.js
@@ -0,0 +1,10 @@
+const name = "bashar bhai";
+const she =  "sanjana tasmin prapti";
+const fname = `this is ${name} and she is ${she}`;
+
+// console.log(fname);
+printName('name','she is mine');
+
+function printName(x,y){
+    console.log(x,y);
+}
